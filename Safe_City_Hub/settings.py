eee2df7c28ec9cbd6c26bd882a0508dbcb3663ab
MikeDtrdev <mikeddoctor08@gmail.com>
2025-05-01 14:10:34 -0800

    update editprofile, viewreport, updatereport

--- Safe_City_Hub/settings.py
+++ Safe_City_Hub/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import pymysql
 import mongoengine
 import os
 from dotenv import load_dotenv
-pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
